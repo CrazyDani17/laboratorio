da2b43a81e3ea31fff177dfc1c960c66f64ac3c1
Daniel <dan13lmch@gmail.com>
2018-09-08 15:58:07 -0500

    añadir codigo de concadenar nombres

---
+++ Laboratorio1/cocadenar_nombres.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string first_name;
+    string second_name;
+    cout << "Ingrese un nombre" << endl;
+    cin >> first_name;
+    cout <<"Ingrese otro nombre"<< endl;
+    cin >> second_name;
+    cout<<first_name+second_name<<endl;
+    return 0;
+}
