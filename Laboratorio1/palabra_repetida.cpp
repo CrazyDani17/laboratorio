1f008e8a02961da3ab22ffb472857a3d1fbbf5d7
Daniel <dan13lmch@gmail.com>
2018-09-08 15:58:07 -0500

    añadir codigo de palabra repetida

---
+++ Laboratorio1/palabra_repetida.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string previous = " ";
+    string current;
+    while(cin>>current){
+        if (previous==current){
+            cout<<"Palabra repetida: "<<current<< endl;
+        }
+        previous=current;
+    }
+    return 0;
+}
